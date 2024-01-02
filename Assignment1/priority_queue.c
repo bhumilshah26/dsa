e2173bccd6538da75667e6835d1734bb35874544
Ansh Shah <122973942+anshshah23@users.noreply.github.com>
2024-01-02 21:09:43 +0530

    Update priority_queue.c

--- Assignment1/priority_queue.c
+++ Assignment1/priority_queue.c
@@ -1,68 +1,71 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #define sn struct Node
 
-struct Node{
+struct Node {
     int data, priority;
     struct Node *next;
-}*start = NULL;
+} *start = NULL;
 
-void insert(){
+void insert() {
     sn *nn, *temp;
-        nn = (sn *)malloc(sizeof(sn));
-        nn -> next = NULL;
-        printf("Enter data: ");
-        scanf("%d",&(nn -> data));
-        printf("Enter Priority: ");
-        scanf("%d",&(nn -> priority));
+    nn = (sn *)malloc(sizeof(sn));
+    nn->next = NULL;
+    printf("Enter data: ");
+    scanf("%d", &(nn->data));
+    printf("Enter Priority: ");
+    scanf("%d", &(nn->priority));
 
-        if(!start || (nn -> priority) < (start -> priority)){
-            nn -> next = start;
-            start = nn;
-        }
-        else {
-            temp = start;
-            while(temp -> next != NULL && temp -> next -> priority <= (nn -> priority))
-                temp = temp -> next;
-            
-            nn -> next = temp -> next;
-            temp -> next = nn;
-        }
+    if (!start || (nn->priority) < (start->priority)) {
+        nn->next = start;
+        start = nn;
+    } else {
         temp = start;
-        while(temp != NULL){
-            temp = temp -> next;
-            printf("%d %d", temp -> data, temp -> priority);
-        }
+        while (temp->next != NULL && temp->next->priority <= (nn->priority))
+            temp = temp->next;
+
+        nn->next = temp->next;
+        temp->next = nn;
+    }
+
+    temp = start;
+    while (temp != NULL) {
+        printf("%d [priority %d] -> ", temp->data, temp->priority);
+        temp = temp->next;
+    }
+    printf("NULL\n");
 }
 
-void del(){
-    if(!start)
-        printf("No elements");
-    else{
+void del() {
+    if (!start)
+        printf("No elements\n");
+    else {
         sn *temp = start;
-        printf("%d is the deleted item", start -> data);
-        start = start -> next;
+        printf("%d is the deleted item\n", start->data);
+        start = start->next;
         free(temp);
     }
 }
 
 void display(sn *temp) {
     if (!temp)
-        printf("Empty Queue");
+        printf("Empty Queue\n");
     else {
-        while(temp != NULL){
-            temp = temp -> next;
-            if (!(temp -> next))
-                printf("%d,[priority %d]", temp -> data, temp -> priority);
+        while (temp != NULL) {
+            if (!(temp->next))
+                printf("%d [priority %d]", temp->data, temp->priority);
             else
-                printf("%d,[priority %d] -> ", temp -> data, temp -> priority);
+                printf("%d [priority %d] -> ", temp->data, temp->priority);
+            temp = temp->next;
         }
+        printf("\n");
     }
 }
 
-int main(){
+int main() {
     int choice;
-    do{
+    do {
         printf("Menu:\n");
         printf("1.Insert\n");
         printf("2.Delete\n");
@@ -70,25 +73,21 @@ int main(){
         printf("4.Exit\n");
         printf("Enter choice: ");
         scanf("%d", &choice);
-        switch(choice){
-            case 1: {
+        switch (choice) {
+            case 1:
                 insert();
                 break;
-            }
-            case 2: {
+            case 2:
                 del();
                 break;
-            }
-            case 3: {
+            case 3:
                 display(start);
                 break;
-            }
-            case 4: 
+            case 4:
                 exit(0);
-
-            default: 
-                printf("Invalid choice");
+            default:
+                printf("Invalid choice\n");
         }
-    } while(choice != 4);
+    } while (choice != 4);
     return 0;
 }
